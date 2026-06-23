ebb71bd5488eac14c9d8f3a3fcb428d62ee37b1e
Gilang <gilangriskymahardhika@gmail.com>
2026-06-23 18:51:16 +0700

    minor update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class IndexController extends Controller
 {
-    function index(Request $request)
+    public function index(Request $request)
     {
-        $input = "https://pendaftaran.elmuna-kebumen.com/asset/img/tanda-tangan.png";
+        $input = 'https://pendaftaran-kursus.elmuna-kebumen.com/asset/img/tanda-tangan.png';
         $qr = QrCode::format('png')
             ->margin(2)
             ->size(115)
-            ->merge(public_path('assets/img/icon1.png'), 0.1, true)
+            ->merge(public_path('assets/img/icon1.png'), 0.3, true)
             ->errorCorrection('H')
             ->generate($input);
 
